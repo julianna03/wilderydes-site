7bcb0b8d12dbcace17a2b702523dac0de0015281
juliannarz <juliannarzhang@gmail.com>
2023-06-14 16:33:30 -0400

    with cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_sSE0pNPay', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4d3dss141ohha0vobs0tbnjtp3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
